be4bb1e70f7914c12041f225d8ca7c62b24f6bc7
md.rahman <md.rahman@mdrahmans-MBP.Dlink>
2021-12-10 18:38:03 +0530

    fix function name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { App } from 'pluggable-app'
 import ReactDOM from 'react-dom'
 
-const bootstrap = (RootComponent: Component, htmlNode: string = 'root') => {
+const bootstrapFn = (RootComponent: Component, htmlNode: string = 'root') => {
     return () => {
         const App = () => <React.StrictMode>
             <RootComponent />
@@ -13,9 +13,9 @@ const bootstrap = (RootComponent: Component, htmlNode: string = 'root') => {
 }
 
 const init = (config: App) => {
-    const bootstrapFn = bootstrap(config.rootComponent, 'root')
+    const bootstrap = bootstrapFn(config.rootComponent, 'root')
     return {
-        bootstrapFn
+        bootstrap
     }
 }
 
